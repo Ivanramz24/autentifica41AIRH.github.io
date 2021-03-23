7cbd9a714b312bb10264127ffa198989da1113ac
Ivanramz24 <81262241+Ivanramz24@users.noreply.github.com>
2021-03-23 14:08:51 -0600

    Create atiti.js

---
+++ js/atiti.js
@@ -0,0 +1,10 @@
+
+firebase.initializeApp({
+apiKey: "AIzaSyBdUlmoL36wO9IkvfbVz0VFeEiAcpgEZLw",
+    authDomain: "autentifica41airh.firebaseapp.com",
+    projectId: "autentifica41airh",
+    storageBucket: "autentifica41airh.appspot.com",
+    messagingSenderId: "23609237530",
+    appId: "1:23609237530:web:56d420a1fa2e21ad9e71a3",
+    measurementId: "G-6T0SGHJG3Y"
+});
